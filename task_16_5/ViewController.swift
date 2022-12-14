776d961176700bf34fba62760cfcbd03972e9ddb
Yulia Evseeva <docs280@gmail.com>
2022-12-14 20:50:26 +0300

    first commit

---
+++ task_16_5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task_16_5
+//
+//  Created by Yulia Evseeva on 14.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
